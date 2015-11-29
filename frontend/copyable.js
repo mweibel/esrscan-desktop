4ec0a35289d31cf7ab3e174556da8105e7300809
Michael Weibel <michael.weibel@gmail.com>
2015-11-29 16:54:12 +0100

    use props text for copying & reset state when going out of the copyable element

--- frontend/copyable.js
+++ frontend/copyable.js
@@ -14,17 +14,22 @@ export default class Copyable extends React.Component {
   }
 
   onClick(el) {
-    let targetEl = el.currentTarget.getElementsByClassName('copyable-text')[0];
-    clipboard.writeText(targetEl.innerHTML.trim());
+    clipboard.writeText(this.props.text.trim());
 
     this.setState({
       copied: true
     });
   }
 
+  onMouseOut() {
+    this.setState({
+      copied: false
+    });
+  }
+
   render() {
     return (
-      <a className="copyable tooltip-link pos-rel dis-inline" onClick={this.onClick.bind(this)} href="#">
+      <a onMouseOut={this.onMouseOut.bind(this)} onClick={this.onClick.bind(this)} className="copyable tooltip-link pos-rel dis-inline" href="#">
         <span className="copyable-text">{this.props.text}</span><i className="pls dis-hidden fa fa-clipboard"></i>
         <span className="mlm txt-small tooltip">{this.state.copied ? translation.copied : translation.copyToClipboard}</span>
       </a>
