c9a890f01a252d98b34e7109c3c84bf3b0f9bf5b
Michael Weibel <michael.weibel@gmail.com>
2016-01-15 21:19:28 +0100

    use gh oauth token for pushing

--- scripts/travis-after-deploy.sh
+++ scripts/travis-after-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-git remote add upstream https://github.com/mweibel/esrscan-desktop.git
+git remote add upstream https://${GH_OAUTH_TOKEN}@github.com/mweibel/esrscan-desktop.git
 git fetch upstream gh-pages
 git checkout gh-pages
 
@@ -14,5 +14,5 @@ git config --global user.name $GIT_AUTHOR_NAME
 git config --global user.email $GIT_AUTHOR_EMAIL
 
 git add index.html
-git commit -m 'Release ${TRAVIS_TAG}: update download links'
+git commit -m "Release ${TRAVIS_TAG}: update download links"
 git push upstream gh-pages
