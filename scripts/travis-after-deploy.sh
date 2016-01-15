d192b8f6a115a8d4d2dec95aa8c33cf6b4eb88d1
Michael Weibel <michael.weibel@gmail.com>
2016-01-15 18:15:46 +0100

    fix deployment script

--- scripts/travis-after-deploy.sh
+++ scripts/travis-after-deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
+git remote add upstream https://github.com/mweibel/esrscan-desktop.git
+git fetch upstream gh-pages
 git checkout gh-pages
 
 url="https://github.com/mweibel/esrscan-desktop/releases/download/${TRAVIS_TAG}/ESRScan.dmg"
@@ -10,4 +12,4 @@ sed -i '' 's#<a href=".*/ESRScan.dmg"#<a href="'$url'"#g' ./index.html
 
 git add index.html
 git commit -m 'Release ${TRAVIS_TAG}: update download links'
-git push origin gh-pages
+git push upstream gh-pages
