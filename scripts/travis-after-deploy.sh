c281f4bb80fc875f9134963fe61728201ca2c6a4
Michael Weibel <michael.weibel@gmail.com>
2016-01-15 18:27:57 +0100

    fix sed match string

--- scripts/travis-after-deploy.sh
+++ scripts/travis-after-deploy.sh
@@ -8,7 +8,7 @@ git checkout gh-pages
 url="https://github.com/mweibel/esrscan-desktop/releases/download/${TRAVIS_TAG}/ESRScan.dmg"
 
 # replace old OSX dmg download link with new one
-sed -i '' 's#<a href=".*/ESRScan.dmg"#<a href="'$url'"#g' ./index.html
+sed -i '' 's#<a href=".*ESRScan.dmg"#<a href="'${url}'"#g' ./index.html
 
 git add index.html
 git commit -m 'Release ${TRAVIS_TAG}: update download links'
