06e23afa95c9a27f11e20053700de29635a221df
Michael Weibel <michael.weibel@gmail.com>
2016-06-01 18:00:40 +0200

    chore(travis): use sudo for linux installs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$ESRSCAN_BUILD" == "windows" ]]; then
-  apt-get install wine makensis gnu-tar
+  sudo apt-get install wine makensis gnu-tar
 fi
 
 if [[ "$ESRSCAN_BUILD" == "linux" ]]; then
