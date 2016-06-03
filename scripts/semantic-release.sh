777670379b7ec1280de93fb9dadb102cb4a40c48
Michael Weibel <michael.weibel@gmail.com>
2016-06-03 08:07:38 +0200

    chore(travis): Run semantic-release on the first worker which is finished
    Without changing this, the deploy scripts of Travis-CI
    would generate the release on GitHub and therefore
    not publish the changelog, only deploy the installer files.

---
+++ scripts/semantic-release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
+if [ -z ${TRAVIS_TAG+x} ]; then
+  echo "-- TRAVIS_TAG is not set, not trying to do semantic-release."
+  exit 0
+fi
+
+URL='https://github.com/mweibel/esrscan-desktop/releases/tag/${TRAVIS_TAG}'
+HTTP_CODE=$(curl --write-out %{http_code} --silent --output /dev/null "$URL")
+
+if [ $HTTP_CODE = "404"]; then
+  echo "-- Release doesn't exist yet, running semantic release."
+  npm run semantic-release-publish
+fi
