b5d1f9c425b0981b208d77d7ead7cc53d6c11589
Michael Weibel <michael.weibel@gmail.com>
2016-06-06 18:20:42 +0200

    chore(package): update related things to electron-builder update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 if [[ "$ESRSCAN_BUILD" == "windows" ]]; then
-  npm run pack:win
   npm run build:win
-  mv ./release/win/ESRScan\ Setup.exe ./release/win/ESRScanSetup.exe
+  mv ./dist/win/ESRScan\ Setup.exe ./dist/win/ESRScanSetup.exe
 fi
 
 if [[ "$ESRSCAN_BUILD" == "linux" ]]; then
-  npm run pack:linux
   npm run build:linux
 fi
 
 if [[ "$ESRSCAN_BUILD" == "osx" ]]; then
-  npm run pack:osx
   npm run build:osx
 fi
--- scripts/travis-before-build.sh
+++ scripts/travis-before-build.sh
@@ -13,6 +13,6 @@ fi
 
 echo "-- Setting builder version for windows & linux to ${TRAVIS_TAG}"
 
-sed -i 's#"version": "dev"#"version": "'${TRAVIS_TAG}'"#g' builder.json
+sed -i 's#"version": "dev"#"version": "'${TRAVIS_TAG}'"#g' package.json
 
 echo "-- Finished update of builder version"
