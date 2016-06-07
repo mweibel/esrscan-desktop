16c26fbc5dc4170cd510220dd30ef7ab7234e26c
Michael Weibel <michael.weibel@gmail.com>
2016-06-07 07:37:32 +0200

    chore(build): Move files to specified place, fix package.json, fix travis.yml

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 if [[ "$ESRSCAN_BUILD" == "windows" ]]; then
+  # wine
   sudo add-apt-repository ppa:ubuntu-wine/ppa -y
+  # mono
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+  echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+
   sudo apt-get update
-  sudo apt-get install wine nsis -y
+  sudo apt-get install --no-install-recommends -y wine1.8 nsis mono-devel ca-certificates-mono osslsigncode
 fi
 
 if [[ "$ESRSCAN_BUILD" == "linux" ]]; then
   gem install fpm
+  sudo apt-get install --no-install-recommends -y icnsutils graphicsmagick xz-utils
 fi
--- scripts/travis-before-build.sh
+++ scripts/travis-before-build.sh
@@ -6,13 +6,18 @@ if [[ "$ESRSCAN_BUILD" == "osx" ]]; then
 fi
 
 # http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
-if [ -z ${TRAVIS_TAG+x} ]; then
+if [ -z ${TRAVIS_TAG+x} ] || [ "$TRAVIS_TAG" == "" ]; then
   echo "-- TRAVIS_TAG is not set, not updating builder version"
   exit 0
 fi
 
 echo "-- Setting builder version for windows & linux to ${TRAVIS_TAG}"
 
-sed -i 's#"version": "0.0.1"#"version": "'${TRAVIS_TAG}'"#g' package.json
+# http://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux
+# sed cross platform compatible needs a `-i.back` instead of just using `-i ''`
+sed -i.bak -e 's#"version": "0.0.1-dev"#"version": "'${TRAVIS_TAG}'"#g' app/package.json
+
+cat app/package.json
+ls -lah app/package*
 
 echo "-- Finished update of builder version"
